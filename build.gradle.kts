3274e006e3c93efbacc6439577e1a2858c891143
kumaresan.anandhan <kumaresan.anandhan@thoughtworks.com>
2026-02-11 19:37:56 +0530

    Enable APM integration with datadog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
-	id("org.springframework.boot") version "4.0.2"
-	id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.spring") version "2.2.21"
+    id("org.springframework.boot") version "4.0.2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.example"
@@ -10,34 +10,47 @@ version = "0.0.1-SNAPSHOT"
 description = "Project for Datadog integration"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("net.logstash.logback:logstash-logback-encoder:7.4")
-	implementation("com.h2database:h2")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+    implementation("com.h2database:h2")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+tasks.bootRun {
+    jvmArgs = listOf(
+        "-javaagent:${project.rootDir}/datadog/dd-java-agent.jar",
+        "-Ddd.service=logexporter",
+        "-Ddd.env=dev",
+        "-Ddd.version=1.0",
+        "-Ddd.logs.injection=true",
+        "-Ddd.trace.agent.host=localhost",
+        "-Ddd.trace.agent.port=8126"
+    )
 }
